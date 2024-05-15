2a7a2bb6a536b66031188547b0fcc1157b09c0d9
Xida Ren (Cedar) <cedar.ren@gmail.com>
2024-05-15 16:53:01 +0000

    use new bend version

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /workspaces
 cd /workspaces
-git clone -b hvm-core https://github.com/HigherOrderCO/bend.git && cd bend && cargo install --path . --locked
-# add alias bend=hvml
-echo "alias bend=hvml" >> ~/.bashrc
+git clone https://github.com/HigherOrderCO/bend.git
+cd bend
+cargo install --path . --locked
